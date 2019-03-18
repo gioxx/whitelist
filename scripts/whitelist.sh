56e76a24d34bc8bb661171e25be7d521f12534dd
Gioxx <dev@gfsolone.com>
2019-03-18 15:34:21 +0100

    Rilascio
    Modifica dello script di Whitelist per richiamare da un solo Updater le
    varie liste, integro nuova stats.txt per i domini che possono tracciare
    alcuni dati della navigazione.

--- scripts/whitelist.sh
+++ scripts/whitelist.sh
@@ -1,34 +1,59 @@
 #!/bin/bash
-# This script will download and add domains from the rep to whitelist.txt file.
-# Project homepage: https://github.com/anudeepND/whitelist
-# Licence: https://github.com/anudeepND/whitelist/blob/master/LICENSE
-# Created by Anudeep (Slight change by cminion)
+# Whitelist Updater for Pi-hole (http://pi-hole.net/)
+# Based on the original script created by Anudeep (Slight change by cminion) - https://github.com/anudeepND/whitelist
+# GSolone (2019) - https://gioxx.org
 #================================================================================
+
 TICK="[\e[32m ✔ \e[0m]"
-
-
-echo -e " \e[1m This script will download and add domains from the repo to whitelist.txt \e[0m"
-sleep 0.1
-echo -e "\n"
 
 if [ "$(id -u)" != "0" ] ; then
 	echo "This script requires root permissions. Please run this as root!"
 	exit 2
 fi
 
-curl -sS https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt | sudo tee -a /etc/pihole/whitelist.txt >/dev/null
-echo -e " ${TICK} \e[32m Adding to whitelist... \e[0m"
-sleep 0.1
-echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
-mv /etc/pihole/whitelist.txt /etc/pihole/whitelist.txt.old && cat /etc/pihole/whitelist.txt.old | sort | uniq >> /etc/pihole/whitelist.txt
-
-echo -e " [...] \e[32m Pi-hole gravity rebuilding lists. This may take a while... \e[0m"
-pihole -g > /dev/null
- 
-echo -e " ${TICK} \e[32m Pi-hole's gravity updated \e[0m"
-echo -e " ${TICK} \e[32m Done! \e[0m"
-
-
-echo -e " \e[1m  Star me on GitHub, https://github.com/anudeepND/whitelist \e[0m"
-echo -e " \e[1m  Happy AdBlocking :)\e[0m"
-echo -e "\n\n"
+case "$1" in
+	safe)
+		echo "Download and add safe sites to whitelist ..."
+		curl -sS https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt | sudo tee -a /etc/pihole/whitelist.txt >/dev/null
+		echo -e " ${TICK} \e[32m Adding to whitelist... \e[0m"
+		sleep 0.1
+		echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
+		mv /etc/pihole/whitelist.txt /etc/pihole/whitelist.txt.old && cat /etc/pihole/whitelist.txt.old | sort | uniq >> /etc/pihole/whitelist.txt
+		echo -e " [...] \e[32m Pi-hole gravity rebuilding lists. This may take a while... \e[0m"
+		pihole -g > /dev/null
+		echo -e " ${TICK} \e[32m Pi-hole's gravity updated \e[0m"
+		echo -e " ${TICK} \e[32m Done! \e[0m"
+		;;
+	referral)
+		echo "Download and add referral sites to whitelist ..."
+		curl -sS https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/referral-sites.txt | sudo tee -a /etc/pihole/whitelist.txt >/dev/null
+		echo -e " ${TICK} \e[32m Adding to whitelist... \e[0m"
+		sleep 0.5
+		echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
+		mv /etc/pihole/whitelist.txt /etc/pihole/whitelist.txt.old && cat /etc/pihole/whitelist.txt.old | sort | uniq >> /etc/pihole/whitelist.txt
+		wait
+		echo -e " [...] \e[32m Pi-hole gravity rebuilding lists...This may take a while \e[0m"
+		pihole -g > /dev/null
+		wait
+		echo -e " ${TICK} \e[32m Pi-hole's gravity updated \e[0m"
+		echo -e " ${TICK} \e[32m Done! \e[0m"
+		;;
+	stats)
+		echo "Download and add statistics sites to whitelist ..."
+		curl -sS https://raw.githubusercontent.com/gioxx/whitelist/master/domains/stats.txt | sudo tee -a /etc/pihole/whitelist.txt >/dev/null
+		echo -e " ${TICK} \e[32m Adding to whitelist... \e[0m"
+		sleep 0.5
+		echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
+		mv /etc/pihole/whitelist.txt /etc/pihole/whitelist.txt.old && cat /etc/pihole/whitelist.txt.old | sort | uniq >> /etc/pihole/whitelist.txt
+		wait
+		echo -e " [...] \e[32m Pi-hole gravity rebuilding lists...This may take a while \e[0m"
+		pihole -g > /dev/null
+		wait
+		echo -e " ${TICK} \e[32m Pi-hole's gravity updated \e[0m"
+		echo -e " ${TICK} \e[32m Done! \e[0m"
+		;;
+	*)
+		echo "Usage: whitelist safe|referral|stats"
+		exit 1
+		;;
+esac
