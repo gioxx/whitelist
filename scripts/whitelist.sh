97b065b2f2a874ce4a6441c21ea8da8ce5e6e2d6
Giovanni <giovanni@solone.it>
2019-03-24 18:26:46 +0100

    Nuove liste e modifica allo script di Whitelist
    Aggiunti i domini suggeriti e verificati in whitelist (vedi https://discourse.pi-hole.net/t/commonly-whitelisted-domains/212/27), isolati i domini legati a Facebook e modificato lo script di download Whitelist che ora prevede le integrazioni e la lista separata per Facebook.

--- scripts/whitelist.sh
+++ scripts/whitelist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Whitelist Updater for Pi-hole (http://pi-hole.net/)
 # Based on the original script created by Anudeep (Slight change by cminion) - https://github.com/anudeepND/whitelist
-# GSolone (2019) - https://gioxx.org
+# GSolone 2019 - https://gioxx.org
 #================================================================================
 
 TICK="[\e[32m ✔ \e[0m]"
@@ -13,12 +13,20 @@ fi
 
 case "$1" in
 	safe)
-		echo "Download and add safe sites to whitelist ..."
+		echo "Download and add safe sites to whitelist (official: anudeepND) ..."
 		curl -sS https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt | sudo tee -a /etc/pihole/whitelist.txt >/dev/null
 		echo -e " ${TICK} \e[32m Adding to whitelist... \e[0m"
 		sleep 0.1
 		echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
 		mv /etc/pihole/whitelist.txt /etc/pihole/whitelist.txt.old && cat /etc/pihole/whitelist.txt.old | sort | uniq >> /etc/pihole/whitelist.txt
+
+		echo "Download and add safe sites to whitelist (integration: gioxx) ..."
+		curl -sS https://raw.githubusercontent.com/gioxx/ph-whitelist/master/domains/whitelist_integrations.txt | sudo tee -a /etc/pihole/whitelist.txt >/dev/null
+		echo -e " ${TICK} \e[32m Adding to whitelist... \e[0m"
+		sleep 0.1
+		echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
+		mv /etc/pihole/whitelist.txt /etc/pihole/whitelist.txt.old && cat /etc/pihole/whitelist.txt.old | sort | uniq >> /etc/pihole/whitelist.txt
+
 		echo -e " [...] \e[32m Pi-hole gravity rebuilding lists. This may take a while... \e[0m"
 		pihole -g > /dev/null
 		echo -e " ${TICK} \e[32m Pi-hole's gravity updated \e[0m"
@@ -52,6 +60,20 @@ case "$1" in
 		echo -e " ${TICK} \e[32m Pi-hole's gravity updated \e[0m"
 		echo -e " ${TICK} \e[32m Done! \e[0m"
 		;;
+	facebook)
+			echo "Download and add Facebook sites to whitelist ..."
+			curl -sS https://raw.githubusercontent.com/gioxx/ph-whitelist/master/domains/whitelist_facebook.txt | sudo tee -a /etc/pihole/whitelist.txt >/dev/null
+			echo -e " ${TICK} \e[32m Adding to whitelist... \e[0m"
+			sleep 0.5
+			echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
+			mv /etc/pihole/whitelist.txt /etc/pihole/whitelist.txt.old && cat /etc/pihole/whitelist.txt.old | sort | uniq >> /etc/pihole/whitelist.txt
+			wait
+			echo -e " [...] \e[32m Pi-hole gravity rebuilding lists. This may take a while... \e[0m"
+			pihole -g > /dev/null
+			wait
+			echo -e " ${TICK} \e[32m Pi-hole's gravity updated \e[0m"
+			echo -e " ${TICK} \e[32m Done! \e[0m"
+			;;
 	*)
 		echo "Usage: whitelist safe|referral|stats"
 		exit 1
