57965092af3a58439b0049d262f4238b26a7194e
Giovanni <giovanni@solone.it>
2019-03-24 18:40:06 +0100

    Correzione dettaglio estetico
    Indentazione

--- scripts/whitelist.sh
+++ scripts/whitelist.sh
@@ -61,28 +61,28 @@ case "$1" in
 		echo -e " ${TICK} \e[32m Done! \e[0m"
 		;;
 	facebook)
-			echo "Download and add Facebook sites to whitelist ..."
-			curl -sS https://raw.githubusercontent.com/gioxx/ph-whitelist/master/domains/whitelist_facebook.txt | sudo tee -a /etc/pihole/whitelist.txt >/dev/null
-			echo -e " ${TICK} \e[32m Adding to whitelist... \e[0m"
-			sleep 0.5
-			echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
-			mv /etc/pihole/whitelist.txt /etc/pihole/whitelist.txt.old && cat /etc/pihole/whitelist.txt.old | sort | uniq >> /etc/pihole/whitelist.txt
-			wait
-			echo -e " [...] \e[32m Pi-hole gravity rebuilding lists. This may take a while... \e[0m"
-			pihole -g > /dev/null
-			wait
-			echo -e " ${TICK} \e[32m Pi-hole's gravity updated \e[0m"
-			echo -e " ${TICK} \e[32m Done! \e[0m"
-			;;
+		echo "Download and add Facebook sites to whitelist ..."
+		curl -sS https://raw.githubusercontent.com/gioxx/ph-whitelist/master/domains/whitelist_facebook.txt | sudo tee -a /etc/pihole/whitelist.txt >/dev/null
+		echo -e " ${TICK} \e[32m Adding to whitelist... \e[0m"
+		sleep 0.5
+		echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
+		mv /etc/pihole/whitelist.txt /etc/pihole/whitelist.txt.old && cat /etc/pihole/whitelist.txt.old | sort | uniq >> /etc/pihole/whitelist.txt
+		wait
+		echo -e " [...] \e[32m Pi-hole gravity rebuilding lists. This may take a while... \e[0m"
+		pihole -g > /dev/null
+		wait
+		echo -e " ${TICK} \e[32m Pi-hole's gravity updated \e[0m"
+		echo -e " ${TICK} \e[32m Done! \e[0m"
+		;;
 	clear)
-			echo "Clean whitelist ..."
-			: > /etc/pihole/whitelist.txt
-			echo -e " [...] \e[32m Pi-hole gravity rebuilding lists. This may take a while... \e[0m"
-			pihole -g > /dev/null
-			wait
-			echo -e " ${TICK} \e[32m Pi-hole's gravity updated \e[0m"
-			echo -e " ${TICK} \e[32m Done! \e[0m"
-			;;
+		echo "Clean whitelist ..."
+		: > /etc/pihole/whitelist.txt
+		echo -e " [...] \e[32m Pi-hole gravity rebuilding lists. This may take a while... \e[0m"
+		pihole -g > /dev/null
+		wait
+		echo -e " ${TICK} \e[32m Pi-hole's gravity updated \e[0m"
+		echo -e " ${TICK} \e[32m Done! \e[0m"
+		;;
 	*)
 		echo "Usage: whitelist safe|referral|stats|facebook|clear"
 		exit 1
