626cb66d351b0fd441d56463495e3f2711d02982
Gioxx <dev@gfsolone.com>
2020-05-11 17:23:47 +0200

    Update whitelist.sh
    Aggiornamento per Pi-Hole 5.0

--- scripts/whitelist.sh
+++ scripts/whitelist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Whitelist Updater for Pi-hole (https://pi-hole.net/)
 # Based on the original script created by Anudeep (Slight change by cminion) - https://github.com/anudeepND/whitelist
-# GSolone - 2019
+# GSolone - 2020
 # Home: https://pihole.noads.it | Blog: https://gioxx.org
 #================================================================================
 
@@ -9,6 +9,8 @@ TICK="[\e[32m ✔ \e[0m]"
 COL_NC="\e[0m" # No Color
 COL_LIGHT_GREEN="\e[1;32m"
 COL_LIGHT_RED="\e[1;31m"
+PIHOLE_LOCATION="/etc/pihole"
+GRAVITY_UPDATE_COMMAND="pihole -w -q"
 
 if [ "$(id -u)" != "0" ] ; then
 	echo "This script requires root permissions. Please run this as root!"
@@ -18,92 +20,92 @@ fi
 case "$1" in
 	safe)
 		echo "Download and add safe sites to whitelist (official: anudeepND) ..."
-		curl -sS https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt | sudo tee -a /etc/pihole/whitelist.txt >/dev/null
+		curl -sS https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt | sudo tee -a "${PIHOLE_LOCATION}"/whitelist.txt >/dev/null
 		echo -e " ${TICK} \e[32m Adding to whitelist... \e[0m"
 		sleep 0.1
 		echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
-		mv /etc/pihole/whitelist.txt /etc/pihole/whitelist.txt.old && cat /etc/pihole/whitelist.txt.old | sort | uniq >> /etc/pihole/whitelist.txt
+		mv "${PIHOLE_LOCATION}"/whitelist.txt "${PIHOLE_LOCATION}"/whitelist.txt.old && cat "${PIHOLE_LOCATION}"/whitelist.txt.old | sort | uniq >> "${PIHOLE_LOCATION}"/whitelist.txt
 
 		echo "Download and add safe sites to whitelist (integration: gioxx) ..."
-		curl -sS https://raw.githubusercontent.com/gioxx/ph-whitelist/master/domains/whitelist_integrations.txt | sudo tee -a /etc/pihole/whitelist.txt >/dev/null
+		curl -sS https://raw.githubusercontent.com/gioxx/ph-whitelist/master/domains/whitelist_integrations.txt | sudo tee -a "${PIHOLE_LOCATION}"/whitelist.txt >/dev/null
 		echo -e " ${TICK} \e[32m Adding to whitelist... \e[0m"
 		sleep 0.1
 		echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
-		mv /etc/pihole/whitelist.txt /etc/pihole/whitelist.txt.old && cat /etc/pihole/whitelist.txt.old | sort | uniq >> /etc/pihole/whitelist.txt
+		mv "${PIHOLE_LOCATION}"/whitelist.txt "${PIHOLE_LOCATION}"/whitelist.txt.old && cat "${PIHOLE_LOCATION}"/whitelist.txt.old | sort | uniq >> "${PIHOLE_LOCATION}"/whitelist.txt
 
 		echo -e " [...] \e[32m Pi-hole gravity rebuilding lists. This may take a while... \e[0m"
-		pihole -g > /dev/null
+		${GRAVITY_UPDATE_COMMAND} $(cat /etc/pihole/whitelist.txt | xargs) > /dev/null
 		echo -e " ${TICK} \e[32m Pi-hole's gravity updated \e[0m"
 		echo -e " ${TICK} \e[32m Done! \e[0m"
 		;;
 	referral)
 		echo "Download and add referral sites to whitelist ..."
-		curl -sS https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/referral-sites.txt | sudo tee -a /etc/pihole/whitelist.txt >/dev/null
+		curl -sS https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/referral-sites.txt | sudo tee -a "${PIHOLE_LOCATION}"/whitelist.txt >/dev/null
 		echo -e " ${TICK} \e[32m Adding to whitelist... \e[0m"
 		sleep 0.5
 		echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
-		mv /etc/pihole/whitelist.txt /etc/pihole/whitelist.txt.old && cat /etc/pihole/whitelist.txt.old | sort | uniq >> /etc/pihole/whitelist.txt
+		mv "${PIHOLE_LOCATION}"/whitelist.txt "${PIHOLE_LOCATION}"/whitelist.txt.old && cat "${PIHOLE_LOCATION}"/whitelist.txt.old | sort | uniq >> "${PIHOLE_LOCATION}"/whitelist.txt
 		wait
 		echo -e " [...] \e[32m Pi-hole gravity rebuilding lists. This may take a while... \e[0m"
-		pihole -g > /dev/null
+		${GRAVITY_UPDATE_COMMAND} $(cat /etc/pihole/whitelist.txt | xargs) > /dev/null
 		wait
 		echo -e " ${TICK} \e[32m Pi-hole's gravity updated \e[0m"
 		echo -e " ${TICK} \e[32m Done! \e[0m"
 		;;
 	stats)
 		echo "Download and add statistics sites to whitelist ..."
-		curl -sS https://raw.githubusercontent.com/gioxx/ph-whitelist/master/domains/stats.txt | sudo tee -a /etc/pihole/whitelist.txt >/dev/null
+		curl -sS https://raw.githubusercontent.com/gioxx/ph-whitelist/master/domains/stats.txt | sudo tee -a "${PIHOLE_LOCATION}"/whitelist.txt >/dev/null
 		echo -e " ${TICK} \e[32m Adding to whitelist... \e[0m"
 		sleep 0.5
 		echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
-		mv /etc/pihole/whitelist.txt /etc/pihole/whitelist.txt.old && cat /etc/pihole/whitelist.txt.old | sort | uniq >> /etc/pihole/whitelist.txt
+		mv "${PIHOLE_LOCATION}"/whitelist.txt "${PIHOLE_LOCATION}"/whitelist.txt.old && cat "${PIHOLE_LOCATION}"/whitelist.txt.old | sort | uniq >> "${PIHOLE_LOCATION}"/whitelist.txt
 		wait
 		echo -e " [...] \e[32m Pi-hole gravity rebuilding lists. This may take a while... \e[0m"
-		pihole -g > /dev/null
+		${GRAVITY_UPDATE_COMMAND} $(cat /etc/pihole/whitelist.txt | xargs) > /dev/null
 		wait
 		echo -e " ${TICK} \e[32m Pi-hole's gravity updated \e[0m"
 		echo -e " ${TICK} \e[32m Done! \e[0m"
 		;;
 	facebook)
 		echo "Download and add Facebook sites to whitelist ..."
-		curl -sS https://raw.githubusercontent.com/gioxx/ph-whitelist/master/domains/whitelist_facebook.txt | sudo tee -a /etc/pihole/whitelist.txt >/dev/null
+		curl -sS https://raw.githubusercontent.com/gioxx/ph-whitelist/master/domains/whitelist_facebook.txt | sudo tee -a "${PIHOLE_LOCATION}"/whitelist.txt >/dev/null
 		echo -e " ${TICK} \e[32m Adding to whitelist... \e[0m"
 		sleep 0.5
 		echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
-		mv /etc/pihole/whitelist.txt /etc/pihole/whitelist.txt.old && cat /etc/pihole/whitelist.txt.old | sort | uniq >> /etc/pihole/whitelist.txt
+		mv "${PIHOLE_LOCATION}"/whitelist.txt "${PIHOLE_LOCATION}"/whitelist.txt.old && cat "${PIHOLE_LOCATION}"/whitelist.txt.old | sort | uniq >> "${PIHOLE_LOCATION}"/whitelist.txt
 		wait
 		echo -e " [...] \e[32m Pi-hole gravity rebuilding lists. This may take a while... \e[0m"
-		pihole -g > /dev/null
+		${GRAVITY_UPDATE_COMMAND} $(cat /etc/pihole/whitelist.txt | xargs) > /dev/null
 		wait
 		echo -e " ${TICK} \e[32m Pi-hole's gravity updated \e[0m"
 		echo -e " ${TICK} \e[32m Done! \e[0m"
 		;;
 	trusted)
 		echo "Download and add safe, referral and stats lists, exclude Facebook ..."
-		curl -sS https://raw.githubusercontent.com/gioxx/ph-whitelist/master/domains/whitelist_integrations.txt | sudo tee -a /etc/pihole/whitelist.txt >/dev/null
-		curl -sS https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/referral-sites.txt | sudo tee -a /etc/pihole/whitelist.txt >/dev/null
-		curl -sS https://raw.githubusercontent.com/gioxx/ph-whitelist/master/domains/stats.txt | sudo tee -a /etc/pihole/whitelist.txt >/dev/null
+		curl -sS https://raw.githubusercontent.com/gioxx/ph-whitelist/master/domains/whitelist_integrations.txt | sudo tee -a "${PIHOLE_LOCATION}"/whitelist.txt >/dev/null
+		curl -sS https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/referral-sites.txt | sudo tee -a "${PIHOLE_LOCATION}"/whitelist.txt >/dev/null
+		curl -sS https://raw.githubusercontent.com/gioxx/ph-whitelist/master/domains/stats.txt | sudo tee -a "${PIHOLE_LOCATION}"/whitelist.txt >/dev/null
 		echo -e " ${TICK} \e[32m Adding to whitelist... \e[0m"
 		sleep 0.5
 		echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
-		mv /etc/pihole/whitelist.txt /etc/pihole/whitelist.txt.old && cat /etc/pihole/whitelist.txt.old | sort | uniq >> /etc/pihole/whitelist.txt
+		mv "${PIHOLE_LOCATION}"/whitelist.txt "${PIHOLE_LOCATION}"/whitelist.txt.old && cat "${PIHOLE_LOCATION}"/whitelist.txt.old | sort | uniq >> "${PIHOLE_LOCATION}"/whitelist.txt
 		wait
 		echo -e " [...] \e[32m Pi-hole gravity rebuilding lists. This may take a while... \e[0m"
-		pihole -g > /dev/null
+		${GRAVITY_UPDATE_COMMAND} $(cat /etc/pihole/whitelist.txt | xargs) > /dev/null
 		wait
 		echo -e " ${TICK} \e[32m Pi-hole's gravity updated \e[0m"
 		echo -e " ${TICK} \e[32m Done! \e[0m"
 		;;
 	demo)
 		echo "Download and add Demo sites to whitelist ..."
-		curl -sS https://raw.githubusercontent.com/gioxx/ph-whitelist/master/domains/HelloWorld.txt | sudo tee -a /etc/pihole/whitelist.txt >/dev/null
+		curl -sS https://raw.githubusercontent.com/gioxx/ph-whitelist/master/domains/HelloWorld.txt | sudo tee -a "${PIHOLE_LOCATION}"/whitelist.txt >/dev/null
 		echo -e " ${TICK} \e[32m Adding to whitelist... \e[0m"
 		sleep 0.5
 		echo -e " ${TICK} \e[32m Removing duplicates... \e[0m"
-		mv /etc/pihole/whitelist.txt /etc/pihole/whitelist.txt.old && cat /etc/pihole/whitelist.txt.old | sort | uniq >> /etc/pihole/whitelist.txt
+		mv "${PIHOLE_LOCATION}"/whitelist.txt "${PIHOLE_LOCATION}"/whitelist.txt.old && cat "${PIHOLE_LOCATION}"/whitelist.txt.old | sort | uniq >> "${PIHOLE_LOCATION}"/whitelist.txt
 		wait
 		echo -e " [...] \e[32m Pi-hole gravity rebuilding lists. This may take a while... \e[0m"
-		pihole -g > /dev/null
+		${GRAVITY_UPDATE_COMMAND} $(cat /etc/pihole/whitelist.txt | xargs) > /dev/null
 		wait
 		echo -e " ${TICK} \e[32m Pi-hole's gravity updated \e[0m"
 		echo -e " ${TICK} \e[32m Done! \e[0m"
@@ -116,9 +118,9 @@ case "$1" in
 		;;
 	clear)
 		echo "Clean whitelist ..."
-		: > /etc/pihole/whitelist.txt
+		: > "${PIHOLE_LOCATION}"/whitelist.txt
 		echo -e " [...] \e[32m Pi-hole gravity rebuilding lists. This may take a while... \e[0m"
-		pihole -g > /dev/null
+		${GRAVITY_UPDATE_COMMAND} $(cat /etc/pihole/whitelist.txt | xargs) > /dev/null
 		wait
 		echo -e " ${TICK} \e[32m Pi-hole's gravity updated \e[0m"
 		echo -e " ${TICK} \e[32m Done! \e[0m"
